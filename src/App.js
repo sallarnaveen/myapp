ab7701be965d63a6cf91f002b4fa9c19e597ca3b
sallarnaveen <hudugurunaveenyadav@gmial.com>
2022-07-20 11:13:00 +0530

    basic app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          NAVEEN CREATED THE REACT PROJECT SET UP SUCCESSFULLY
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <button>CLICK ME</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
